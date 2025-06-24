3e62c61c58f1abcc364c00a7c6dd5f35e7df9c8b
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-06-24 18:58:12 +0530

    Work Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 const App = () => {
   return (
-    <div className='w-full h-screen font-["Satoshi-Variable"] bg-zinc-900'>
+    <div className='w-full h-screen font-["Satoshi Variable"] bg-zinc-900'>
       <Navbar />
+      <Work /> 
     </div>
   )
 }
