d66697f690865bbc86e851038ba579683343c91b
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-06-26 18:30:12 +0530

    Card Completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { IoIosArrowRoundForward } from 'react-icons/io';
 
-const Card = () => {
+const Card = ({width, start, para, hover}) => {
   return (
-    <div className="w-1/2 p-5 rounded-xl text-zinc-100 bg-zinc-800">
+    <div className={`p-5 rounded-xl text-zinc-100 bg-zinc-800 ${width} min-h-[60vh] flex flex-col justify-between hover:${hover}`}>
       <div className="w-full">
         <div className="w-full flex justify-between">
           <h1>Heading</h1>
@@ -11,7 +11,25 @@ const Card = () => {
         </div>
         <h1 className="text-3xl font-medium mt-5">Heading 2</h1>
       </div>
-      
+      <div className="w-full">
+        {start === true && (
+          <>
+            <h1 className="text-6xl font-semibold tracking-tight">
+              Start a Project
+            </h1>
+            <button className="rounded-full px-4 py-3 border-1 border-zinc-100 mt-5">
+              Contact Us
+            </button>
+          </>
+        )}
+        {para === true && (
+          <>
+            <p className="text-sm opacity-40 mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,13 +3,23 @@ import Card from './Card'
 
 const Cards = () => {
   return (
-      <div className='w-full bg-zinc-900'>
-          <div className='max-w-screen-xl mx-auto py-32 flex gap-3'>
-              <Card />
-              <Card />
-          </div>
+    <div className="w-full bg-zinc-900">
+      <div className="max-w-screen-xl mx-auto py-32 flex gap-3">
+        <Card
+          width={"basis-1/3"}
+          start={false}
+          para={true}
+          hover={"bg-[#3c3a43]"}
+        />
+        <Card
+          width={"basis-2/3"}
+          start={true}
+          para={false}
+          hover={"bg-[#4c20ad]"}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cards
