7e1887222d4c515396c1cc20ccf05bb54e0f1d08
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-06-26 20:27:34 +0530

    Card Updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 const Cards = () => {
   return (
     <div className="w-full bg-zinc-900">
-      <div className="max-w-screen-xl mx-auto py-32 flex gap-3">
+      <div className="max-w-screen-xl mx-auto py-32 flex gap-2">
         <Card
           width={"basis-1/3"}
           start={false}
