d0279428370b12e6ef4e018228188e46d4b696ac
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-06-27 12:33:20 +0530

    Locomotive for Smooth Scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Work from './components/Work'
-import Stripes from './components/Stripes'
-import Projects from './components/Projects'
-import Marquees from './components/Marquees'
-import Cards from './components/Cards'
-import Footer from './components/Footer'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Work from './components/Work';
+import Stripes from './components/Stripes';
+import Projects from './components/Projects';
+import Marquees from './components/Marquees';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
+import LocomotiveScroll from "locomotive-scroll";
 
 const App = () => {
+
+  const locomotiveScroll = new LocomotiveScroll();
+
   return (
     <div className='w-full h-screen font-["Satoshi Variable"] bg-zinc-900 text-zinc-100'>
       <Navbar />
